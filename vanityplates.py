6cb5cd229939752a722a410f6dc5d297ccb4bc61
k444o1 <153091428+k444o1@users.noreply.github.com>
2024-02-23 19:49:48 +0800

    Create vanityplates.py

---
+++ vanityplates.py
@@ -0,0 +1,34 @@
+import string
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+def is_valid(s):
+    if s[0:2].isalpha():
+        pass
+    else:
+        return False
+    if 2 <= len(s) <= 6:
+        pass
+    else:
+        return False
+    if s.isalpha():
+        return True
+    for character in s:
+        if character in string.punctuation:
+            return False
+    for character in s:
+        if character.isdigit():
+            break #return stops execution of function
+    if character == "0":
+        return False
+    if s[s.index(character): ].isdigit(): #checking if all characters after non-zero number are digits
+        return True
+    else:
+        return False
+
+
+main()
+
