24566746702ffc732043b730119ad729b10f0f16
k444o1 <153091428+k444o1@users.noreply.github.com>
2024-03-25 17:28:56 +0800

    Add files via upload

---
+++ cokemachine.py
@@ -0,0 +1,11 @@
+amount = 0
+due = 50 - amount
+while amount < 50:
+    print(f"Amount Due: {due}")
+    coin = int(input("Insert Coin: "))
+    if coin == 25 or coin == 10 or coin == 5: #reprompts user if coin is not 25/10/5
+        amount += coin
+        due = 50 - amount
+        if due <= 0: #due is either negative(meaning there's change owed) or 0(no change owed)
+            change = -1 * due
+            print(f"Change Owed: {change}") 
